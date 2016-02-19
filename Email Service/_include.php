cc7927b620fc9c0e6d0f609557ab08ee77316602
brianchristopherpeck <brianchristopherpeck@gmail.com>
2016-02-19 01:16:35 -0500

    Added Email Service Folder, _include file and MySQL database emailService and table subscribers with id as primary key and email as unique to avoid duplicates

---
+++ Email Service/_include.php
@@ -0,0 +1,27 @@
+<?php
+// emailService include functions //
+function doDB()
+{
+	global $mysqli;
+
+	// connect to the server and select database
+	$mysqli = mysqli_connect("localhost", "root", "@wesome", "emailService");
+
+	// connection error
+	if(mysqli_connect_errno())
+	{
+		printf("Connection failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+}
+
+function emailChecker($email) 
+{
+	global $mysqli, $safe_email, $check_res;
+
+	// check that email doesn't already exist
+	$safe_email = mysqli_real_excape_string($mysqli, $email);
+	$check_sql = "SELECT id FROM subscribers WHERE email = '".$safe_email."'";
+	$check_res = mysqli_query($mysqli, $check_sql) or die(mysqli_error($mysqli));
+}
+?>
